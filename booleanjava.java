8321099e61ea2a6cba18ccd8322dde9175bd60b9
PrabhulingBiradar <prabulingabiradar537.xworkz@gmail.com>
2021-12-21 10:48:13 +0530

    learning booleanjava

---
+++ booleanjava.java
@@ -0,0 +1,21 @@
+class booleanjava
+{
+public static void main(String[]args){
+
+
+boolean[] value={true,false,true,false,false,true,true,true,true,true};
+   System.out.println(value[0]);
+   System.out.println(value[1]);
+ System.out.println(value[2]);
+ System.out.println(value[3]);
+ System.out.println(value[4]);
+ System.out.println(value[5]);
+ System.out.println(value[6]);
+ System.out.println(value[7]);
+ System.out.println(value[8]);
+ System.out.println(value[9]);
+
+
+
+}
+}
